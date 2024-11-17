b800ec0f8238f8d83318140f1b0f03f13a95afea
Trymnyheim <trym.haakon.nyheim@gmail.com>
2024-11-17 18:12:35 +0100

    Update UNSOLVED_coloring_graphs.py

--- Kattis/UNSOLVED_coloring_graphs.py
+++ Kattis/UNSOLVED_coloring_graphs.py
@@ -1,30 +1,28 @@
+# https://open.kattis.com/problems/coloring
+
 from sys import stdin
 from collections import defaultdict
 
 def main():
+    # Get and process all input:
     N = int(next(stdin))
-    G = Graph()
-    while N > 0:
-        input = [int(x) for x in next(stdin).strip().split(" ")]
-        vertex = input[0]
-        edges = input[1:]
-        G.addVertex(vertex, edges)
-        N -= 1
-    G.print_graph()
-
-
-class Graph:
-    def __init__(self):
-        self.V = set()
-        self.E = defaultdict(set)
-
-    def addVertex(self, val, edges):
-        self.V.add(val)
-        for u in edges:
-            self.V.add(u)
-            self.E[val].add(u)
-            self.E[u].add(val)
-        
+    data = [] # Collects all input
+    vertices = set()
+    for _ in range(N):
+        input = [int(x) for x in next(stdin).strip().split()]
+        data.append(input)
+        [vertices.add(x) for x in input]
+    
+    # Build graph:
+    graph = dict()
+    for v in vertices:
+        graph[v] = set()
+    for line in data:
+        if len(line) > 1:
+            v = line[0]
+            for u in line[1:]:
+                graph[v].add(u)
+    print(graph)
 
 if __name__ == "__main__":
     main()
